8b5d6ee34199e74661f840eacd5ffe3a49bb41aa
James Biggs <jamesbiggs154@gmail.com>
2020-10-20 12:17:14 +0100

    Created new routes and basic HTML pages to route to

--- app.py
+++ app.py
@@ -6,3 +6,15 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template("home.html")
+
+@app.route('/help')
+def help():
+    return render_template("help.html")
+
+@app.route('/seat-select')
+def seatselect():
+    return render_template("seatselect.html")
+
+@app.route('/idk-help')
+def idkhelp():
+    return render_template("idkhelp.html")
