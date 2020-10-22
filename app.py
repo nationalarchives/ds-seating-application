dd63f75b7da76e38ae85ef3b83a962a1e2f2fee7
James Biggs <jamesbiggs154@gmail.com>
2020-10-22 13:02:33 +0100

    DRY templating and minor CSS changes

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-@app.route('/help')
-def help():
-    return render_template("help.html")
+@app.route('/help-choosing')
+def helpchoosing():
+    return render_template("helpchoosing.html")
 
 @app.route('/seat-select')
 def seatselect():
     return render_template("seatselect.html")
 
-@app.route('/idk-help')
-def idkhelp():
-    return render_template("idkhelp.html")
+@app.route("/does-not-know-if-help-choosing-seat-is-needed")
+def idontknowhelp():
+    return render_template("idontknowhelp.html")
