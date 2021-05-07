43d0c5c5c39398a9c8c60cf87dd21645c62177c1
James Biggs <jamesbiggs154@gmail.com>
2021-05-07 13:05:26 +0100

    Added alt tag to logo img, made questions required

--- static/script.js
+++ static/script.js
@@ -1,15 +1,19 @@
 var check_researching_by_self = function () {
-    var ischecked = $("#researchingbyself_yes").is(":checked")
+    var ischecked = $("#researchingbyself_yes").is(":checked");
     if (ischecked) {
-        $("#quietzone").show()
+        $("#quietzone").show();
+        $("#quietzone_yes").prop("disabled", false);
+        $("#quietzone_no").prop("disabled", false);
     } else {
-        $("#quietzone").hide()
-        $("#quietzone_yes").prop("checked", false)
-        $("#quietzone_no").prop("checked", false)
+        $("#quietzone").hide();
+        $("#quietzone_yes").prop("checked", false);
+        $("#quietzone_no").prop("checked", false);
+        $("#quietzone_yes").prop("disabled", true);
+        $("#quietzone_no").prop("disabled", true);
     }
 }
 $(document).ready(function () {
-    check_researching_by_self()
+    check_researching_by_self();
 });
 $("#researchingbyself").on('change', function () {
     check_researching_by_self();
