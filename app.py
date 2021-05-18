2aae2bc33cf9b3b808dd098d235c7be00eb46636
James Biggs <jamesbiggs154@gmail.com>
2021-05-18 17:28:26 +0100

    Added confirm/decline for seat recommendation page

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import redirect
 import sqlite3
 import random
 
@@ -17,6 +18,16 @@ def termsandconditions():
 @app.route('/home')
 def home():
     return render_template("home.html")
+
+def getSeatLength(seatList):
+    count = 0
+    for i in seatList:
+        count += 1
+    return count
+
+@app.route('/seatchosen', methods=["POST"])
+def seatchosen():
+    bookedSeat = request.form["seat"]
 
 
 @app.route('/helpchoosing', methods=["GET","POST"])
@@ -49,20 +60,28 @@ def helpchoosing():
         accessType = accessrequirements
 
 
-        availableSeats = cur.execute(f"SELECT * FROM seats WHERE zoneID='{zone}' AND hasLight={hasLight} AND accessType='{accessType}' AND isBooked=0").fetchall()
+        availableSeats = cur.execute(f"SELECT * FROM seats WHERE roomID='{roomID}' AND zoneID='{zone}' AND hasLight={hasLight} AND accessType='{accessType}' AND isBooked=0").fetchall()
         
-        count = 0
-        for i in availableSeats:
-            count += 1
+        count = getSeatLength(availableSeats)
+        while count == 0:
+            availableSeats = cur.execute(f"SELECT * FROM seats WHERE roomID='{roomID}' AND zoneID='{zone}' AND accessType='{accessType}' AND isBooked=0").fetchall()
+            count = getSeatLength(availableSeats)
+            if count == 0:
+                availableSeats = cur.execute(f"SELECT * FROM seats WHERE roomID='{roomID}' AND accessType='{accessType}' AND isBooked=0").fetchall()
+                count = getSeatLength(availableSeats)
+                if count == 0:
+                    availableSeats = cur.execute(f"SELECT * FROM seats WHERE roomID='{roomID}' AND isBooked=0").fetchall()
+                    count = getSeatLength(availableSeats)
+                    if count == 0:
+                        return render_template("noseats.html")
+                
         randomSeat = availableSeats[random.randint(0, count-1)]
-
+        
         selectedSeat = randomSeat[0]
         selectedRoom = randomSeat[2]
         selectedZone = randomSeat[3]
 
-
-        
-        con.close() 
+        con.close()
         return render_template("recommended.html", seatID = selectedSeat, roomID = selectedRoom, zoneID = selectedZone)
     else:
         return render_template("helpchoosing.html", request=request)
