81ad939448919645c0f4e63b5378e298529ab439
James Biggs <jamesbiggs154@gmail.com>
2021-10-06 13:54:49 +0100

    Slight accessibility changes

--- app.py
+++ app.py
@@ -22,16 +22,20 @@ def readersnumber():
             return render_template("readersnumber.html", errorMessage="Invalid reader's number: invalid format")
         if len(givenReaders) != 6:
             return render_template("readersnumber.html", errorMessage="Invalid reader's number: invalid format")
+
         con = sqlite3.connect('readersnumber.db')
         cur = con.cursor()
         readersSearch = cur.execute(f"SELECT * FROM readers WHERE readersNumber={givenReaders}").fetchall()
         con.close()
+
         if len(readersSearch) == 0:
             return render_template("readersnumber.html", errorMessage="Invalid reader's number: reader's number not found")
+
         global readersNumber
         readersNumber = readersSearch[0][0]
         global readersName
         readersName = readersSearch[0][1]
+        
         return render_template("home.html")
     return render_template("readersnumber.html", errorMessage="")
 
@@ -44,18 +48,21 @@ def getSeatLength(seatList):
 @app.route('/seatchosen', methods=["POST"])
 def seatchosen():
     bookedSeat = request.form["seat"]
+
     con = sqlite3.connect('seatbooking.db')
     cur = con.cursor()
     cur.execute(f"UPDATE seats SET isBooked=1 WHERE seatID='{bookedSeat}'")
     cur.execute(f"UPDATE seats SET bookingHolder={readersNumber} WHERE seatID='{bookedSeat}'")
     con.commit()
     con.close()
+
     con2 = sqlite3.connect('readersnumber.db')
     cur2 = con2.cursor()
     cur2.execute(f"UPDATE readers SET readersSeatID='{bookedSeat}' WHERE readersNumber={readersNumber}")
     cur2.execute(f"UPDATE readers SET hasBooking=1 WHERE readersNumber={readersNumber}")
     con2.commit()
     con2.close()
+
     return render_template("confirmation.html", userSeat=bookedSeat)
 
 
